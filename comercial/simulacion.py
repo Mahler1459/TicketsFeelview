49cb40233ad64da19ac567e71bc729d023d4b3c5
Mahler1459 <agustinderuschi@hotmail.com>
2021-03-15 09:11:41 -0300

    fix de porcentajes

--- comercial/simulacion.py
+++ comercial/simulacion.py
@@ -19,22 +19,36 @@ def getSimulacion2(transacciones, cajero):
 		comisiones = \
 		[int(a*b*c) for a,b,c in zip(data["montos_com_disp"],data["comisiones_disp"],operaciones)]
 		monto = data["montos_vault_disp"]
+		interv = data["int_disp"]
+		por = data["por_disp"]
 	else:
 		operaciones = [int(transacciones * a * b) for a,b in zip(data["porcentajes"],data["op_rec"])]
 		comisiones = \
 		[int(a*b*c) for a,b,c in zip(data["montos_com_rec"],data["comisiones_rec"],operaciones)]
 		monto = data["montos_vault_rec"]
+		interv = data["int_rec"]
+		por = data["por_rec"]
 
 	vault = "$ {:,}".format(sum([ m*op for m,op in zip(monto, operaciones)])).replace(",",".")
 	totalComisiones = sum(comisiones)
 	totalOperaciones = sum(operaciones)
+
+	if totalOperaciones < interv[0]:
+		mult = por[0]
+	elif totalOperaciones > interv[0] and totalOperaciones < interv[1]:
+		mult = por[1]
+	else:
+		mult = por[2]
+
 	porcentaje = [round(a/totalComisiones*100,1) for a in comisiones]
 	TotalAnual = 12*sum(comisiones)
 
 	resultados = {}
-	resultados['Inversor'] = "$ {:,}".format(int(TotalAnual*0.3)).replace(",",".")
-	resultados['Establecimiento'] = "$ {:,}".format(int(TotalAnual*0.2)).replace(",",".")
-	resultados['Negocio Completo'] = "$ {:,}".format(int(TotalAnual*0.5)).replace(",",".")
+	resultados['Inversor'] = 			"$ {:,}".format(int(TotalAnual*0.3)).replace(",",".")
+	resultados['Establecimiento'] = 	"$ {:,}".format(int(TotalAnual*mult)).replace(",",".")
+	resultados['Negocio Completo'] = 	"$ {:,}".format(int(TotalAnual*(0.3+mult))).replace(",",".")
+
+	porNegocio = ["{:,}".format(0.3).replace(",","."), "{:,}".format(mult).replace(",","."), "{:,}".format(round(1-(0.3+mult),2)).replace(",",".")]
 	
 	return operaciones, comisiones, resultados, nombres, "{:,}".format(totalOperaciones).replace(",","."),\
-	 data["porNegocio"], monto, porcentaje, "{:,}".format(totalComisiones).replace(",","."), vault
+	 porNegocio, monto, porcentaje, "{:,}".format(totalComisiones).replace(",","."), vault
